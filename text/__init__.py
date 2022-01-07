5d6ce890b4fa240122dff838bcfa0cfba7a7ad13
Isaac L. Bleaman <bleaman@berkeley.edu>
2022-01-07 13:13:25 -0500

    Don't print utt during training

--- text/__init__.py
+++ text/__init__.py
@@ -29,7 +29,6 @@ def text_to_sequence(text, cleaner_names):
 
     # Check for curly braces and treat their contents as ARPAbet:
     while len(text):
-        print(f"text: {text}")
         m = _curly_re.match(text)
 
         if not m:
